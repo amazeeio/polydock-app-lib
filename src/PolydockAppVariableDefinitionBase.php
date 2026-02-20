cf234a3d6722cd01f0296e0fc01c34e3553c9340
Dan Lemon <daniel.lemon@amazee.io>
2026-02-20 13:14:55 +0100

    chore: add docblock & update deps

--- src/PolydockAppVariableDefinitionBase.php
+++ src/PolydockAppVariableDefinitionBase.php
@@ -6,11 +6,22 @@ class PolydockAppVariableDefinitionBase implements PolydockAppVariableDefinition
 {
     public function __construct(protected string $name) {}
 
+    /**
+     * Get the name of the app variable definition
+     *
+     * @return string The name
+     */
     public function getName(): string
     {
         return $this->name;
     }
 
+    /**
+     * Set the name of the app vairable definition
+     *
+     * @param string $name
+     * @return PolydockAppVariableDefinitionBase
+     */
     public function setName(string $name): self
     {
         $this->name = $name;
